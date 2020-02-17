544518505b00f73651a289f55a008185eb66e0b7
marouenbg <marouen.b.guebila@gmail.com>
2020-02-17 02:10:03 +0000

    added memory results

---
+++ memoryBenchmark.m
@@ -0,0 +1,20 @@
+exp_file   = 'test_data/expression.txt';
+motif_file = 'test_data/motifTest.txt';
+ppi_file   = 'test_data/ppi.txt';
+tic;[AgNet2,cpuImpl] = panda_run('',exp_file, motif_file, ppi_file, '',...
+                    '', 0.1, 0, 'union',0.5,0,'Tfunction','gpu','double');toc;
+                
+tic;[AgNet2,gpuImpl] = panda_run('',exp_file, motif_file, ppi_file, '',...
+                    '', 0.1, 0, 'union',0.5,0,'Tfunction','gpu','double');toc;
+                
+                
+cpuMem=[cpuImpl.MemAvailableAllArrays];
+cpuRes=cpuMem-cpuMem(1);
+
+gpuMem=[gpuImpl.MemAvailableAllArrays];
+gpuRes=gpuMem-gpuMem(1);
+figure;
+hold on
+bar(-cpuRes)
+bar(-gpuRes)
+legend('cpu','gpu')
