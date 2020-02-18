be225135ba98bf5ce5180a57f66a8035bd1d8b5c
marouenbg <marouen.b.guebila@gmail.com>
2020-02-17 19:46:23 -0500

    recomputing memory

--- memoryBenchmark.m
+++ memoryBenchmark.m
@@ -1,11 +1,27 @@
 exp_file   = 'test_data/expression.txt';
 motif_file = 'test_data/motifTest.txt';
 ppi_file   = 'test_data/ppi.txt';
-tic;[AgNet2,cpuImpl] = panda_run('',exp_file, motif_file, ppi_file, '',...
-                    '', 0.1, 0, 'union',0.5,0,'Tfunction','gpu','double');toc;
-                
-tic;[AgNet2,gpuImpl] = panda_run('',exp_file, motif_file, ppi_file, '',...
-                    '', 0.1, 0, 'union',0.5,0,'Tfunction','gpu','double');toc;
+
+[Exp,RegNet,TFCoop,TFNames,GeneNames]=processData(exp_file,motif_file,ppi_file,modeProcess);
+
+disp('Computing coexpression network:');
+tic; GeneCoReg = Coexpression(Exp); toc;
+
+disp('Normalizing Networks:');
+tic
+    RegNet    = NormalizeNetwork(RegNet);
+    GeneCoReg = NormalizeNetwork(GeneCoReg);
+    TFCoop    = NormalizeNetwork(TFCoop);
+toc
+
+disp('Running PANDA algorithm:');
+verbose=0;precision='double';computing='cpu';similarityMetric='Tfunction';
+respWeight=0.5;alpha=0.1;
+[AgNet,gpuImpl] = gpuPANDA(RegNet, GeneCoReg, TFCoop, alpha, respWeight,...
+    similarityMetric, computing, precision, verbose);
+
+[AgNet,cpuImpl] = PANDA(RegNet, GeneCoReg, TFCoop, alpha, respWeight,...
+    similarityMetric, computing, precision, verbose);
                 
 %%       
 cpuMem=[cpuImpl.MemAvailableAllArrays];
