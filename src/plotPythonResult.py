cab62b17c1ae3c6a98f34656078ec337f49ec7c6
marouenbg <marouen.b.guebila@gmail.com>
2021-06-03 15:19:55 -0400

    updating with new gpu

--- src/plotPythonResult.py
+++ src/plotPythonResult.py
@@ -4,15 +4,15 @@ import pandas as pd
 import os
 
 #Load data
-os.chdir('../../gpupanda')
+os.chdir('/Users/mab8354/projects/gpupanda')
 python_gpu2_times= [.10, .11, .10] #single precision, alpha=0.1, Tfunction
 python_gpu2_time = np.mean(python_gpu2_times)
 python_cpu2_times = [1.77, 1.55, 1.66] #single precision, alpha=0.1, Tfunction
 python_cpu2_time = np.mean(python_cpu2_times)
 
 #Check that (small, alpha=0.1, single precision) networks are identical in gpuPANDA and PANDA in python
-singleSmallGPU=pd.read_csv('data/single_gpu_panda.txt',sep=' ', header=None)
-singleSmallCPU=pd.read_csv('data/single_cpu_panda.txt',sep=' ', header=None)
+singleSmallGPU=pd.read_csv('data/Python/single_gpu_panda.txt',sep=' ', header=None)
+singleSmallCPU=pd.read_csv('data/Python/single_cpu_panda.txt',sep=' ', header=None)
 np.max(np.max(np.abs(singleSmallGPU-singleSmallCPU))) #3.500000000045134e-05
 
 #Plot figure
@@ -20,7 +20,7 @@ plt.rcParams["font.family"] = "Arial"
 plt.subplot(122)
 plt.plot(singleSmallGPU.values.flatten(), singleSmallCPU.values.flatten(), 'o',
          markeredgewidth=2, alpha=0.5)
-plt.text(15, 0, '3.5e-5', fontsize=12)
+plt.text(5, 0, 'Maxdiff = $3.5*10^{-5}$', fontsize=12)
 plt.xlabel('gpuPANDA edge weights')
 plt.ylabel('PANDA edge weights')
 plt.plot([-6,26],[-6,26])
@@ -30,15 +30,16 @@ plt.ylabel('Run time (s)')
 plt.xticks([1,2], ('gpuPANDA (Python)', 'PANDA (Python)'))
 
 #Plot gpuLIONESS
-gpu1_lioness=pd.read_csv('data/results/lioness/LIONESS_gpu_gpu1_resTable.csv')
-cpu2_lioness=pd.read_csv('data/results/lioness/LIONESS_cpu_cpu2_resTable.csv')
-lionessGPU=pd.read_csv('data/results/lioness/gpuLIONESS.csv',header=None)
-lionessCPU=pd.read_csv('data/results/lioness/cpuLIONESS.csv',header=None)
+gpu1_lioness=pd.read_csv('data/MATLAB/lioness/LIONESS_gpu_gpu1_resTable.csv')
+cpu2_lioness=pd.read_csv('data/MATLAB/lioness/LIONESS_cpu_cpu2_resTable.csv')
+lionessGPU=pd.read_csv('data/MATLAB/lioness/gpuLIONESS.csv',header=None)
+lionessCPU=pd.read_csv('data/MATLAB/lioness/cpuLIONESS.csv',header=None)
 np.max(np.max(np.abs(lionessGPU-lionessCPU)))
 plt.subplot(122)
 plt.plot(lionessGPU.values.flatten(), lionessCPU.values.flatten(), 'o',
          markeredgewidth=2, alpha=0.5)
-plt.text(15, -50, '0.015', fontsize=12)
+#plt.text(15, -50, 'Maxdiff = 0.015', fontsize=12)
+#plt.text(15, -70, 'Avdiff = $6.5ex10^{5}$', fontsize=12)
 plt.xlabel('gpuLIONESS edge weights')
 plt.ylabel('LIONESS edge weights')
 plt.plot([-60,110],[-60,110])
